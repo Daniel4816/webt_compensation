29230753da0ffeac9c5339da0f7f44f9fc925ba7
Daniel Schuecker <42698416+Daniel4816@users.noreply.github.com>
2025-09-14 15:27:02 +0200

    Initial commit

---
+++ files/main.js
@@ -0,0 +1,247 @@
+/* A builder class to simplify the task of creating HTML elements */
+class ElementCreator {
+    constructor(tag) {
+        this.element = document.createElement(tag);
+    }
+
+    id(id) {
+        this.element.id = id;
+        return this;
+    }
+
+    class(clazz) {
+        this.element.class = clazz;
+        return this;
+    }
+
+    text(content) {
+        this.element.innerHTML = content;
+        return this;
+    }
+
+    with(name, value) {
+        this.element.setAttribute(name, value)
+        return this;
+    }
+
+    listener(name, listener) {
+        this.element.addEventListener(name, listener)
+        return this;
+    }
+
+    append(child) {
+        child.appendTo(this.element);
+        return this;
+    }
+
+    prependTo(parent) {
+        parent.prepend(this.element);
+        return this.element;
+    }
+
+    appendTo(parent) {
+        parent.append(this.element);
+        return this.element;
+    }
+
+    insertBefore(parent, sibling) {
+        parent.insertBefore(this.element, sibling);
+        return this.element;
+    }
+
+    replace(parent, sibling) {
+        parent.replaceChild(this.element, sibling);
+        return this.element;
+    }
+}
+
+/* A class representing a resource. This class is used per default when receiving the
+   available resources from the server (see end of this file).
+   You can (and probably should) rename this class to match with whatever name you
+   used for your resource on the server-side.
+ */
+class Animal {
+
+    /* If you want to know more about this form of getters, read this:
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get */
+    get idforDOM() {
+        return `resource-${this.id}`;
+    }
+
+}
+
+function add(resource, sibling) {
+
+    const creator = new ElementCreator("article")
+        .id(resource.idforDOM);
+
+    /* Task 2: Instead of the name property of the example resource, add the properties of
+       your resource to the DOM. If you do not have the name property in your resource,
+       start by removing the h2 element that currently represents the name. For the 
+       properties of your object you can use whatever html element you feel represents
+       your data best, e.g., h2, paragraphs, spans, ... 
+       Also, you don't have to use the ElementCreator if you don't want to and add the
+       elements manually. */
+
+    creator
+        .append(new ElementCreator("h2").text(resource.name))
+    creator.append(new ElementCreator("p").text("Species: " + resource.species))
+    creator.append(new ElementCreator("p").text("Age: " + resource.age))
+    creator.append(new ElementCreator("p").text("Birthday: " + resource.birthday))
+    creator.append(new ElementCreator("p").text("Domestic: " + resource.isDomestic))
+
+    creator
+        .append(new ElementCreator("button").text("Edit").listener('click', () => {
+            edit(resource);
+        }))
+        .append(new ElementCreator("button").text("Remove").listener('click', () => {
+            /* Task 3: Call the delete endpoint asynchronously using either an XMLHttpRequest
+               or the Fetch API. Once the call returns successfully, remove the resource from
+               the DOM using the call to remove(...) below. */
+            const xhr = new XMLHttpRequest();
+            xhr.open('DELETE', `/api/resources/${resource.id}`);
+            xhr.onload = () => {
+                if (xhr.status === 204) {
+                    remove(resource); // <- This call removes the resource from the DOM. Call it after (and only if) your API call succeeds!
+                }
+            };
+            xhr.send();
+        }));
+
+    const parent = document.querySelector('main');
+
+    if (sibling) {
+        creator.replace(parent, sibling);
+    } else {
+        creator.insertBefore(parent, document.querySelector('#bottom'));
+    }
+        
+}
+
+function edit(resource) {
+    const formCreator = new ElementCreator("form")
+        .id(resource.idforDOM)
+        .append(new ElementCreator("h3").text("Edit " + resource.name));
+    
+    /* Task 4 - Part 1: Instead of the name property, add the properties your resource has here!
+       The label and input element used here are just an example of how you can edit a
+       property of a resource, in the case of our example property name this is a label and an
+       input field. Also, we assign the input field a unique id attribute to be able to identify
+       it easily later when the user saves the edited data (see Task 4 - Part 2 below). 
+    */
+
+    formCreator
+        .append(new ElementCreator("label").text("Name").with("for", "resource-name"))
+        .append(new ElementCreator("input").id("resource-name").with("type", "text").with("value", resource.name))
+        .append(new ElementCreator("label").text("Species").with("for", "resource-species"))
+        .append(new ElementCreator("input").id("resource-species").with("type", "text").with("value", resource.species))
+        .append(new ElementCreator("label").text("Age").with("for", "resource-age"))
+        .append(new ElementCreator("input").id("resource-age").with("type", "text").with("value", resource.age))
+        .append(new ElementCreator("label").text("Birthday").with("for", "resource-birthday"))
+        .append(new ElementCreator("input").id("resource-birthday").with("type", "date").with("value", new Date(resource.birthday).toISOString().slice(0, 10)))
+        .append(new ElementCreator("label").text("Domestic").with("for", "resource-domestic"))
+        .append(resource.isDomestic
+            ? new ElementCreator("input").id("resource-domestic").with("type", "checkbox").with("checked", "")
+            : new ElementCreator("input").id("resource-domestic").with("type", "checkbox")
+        );
+//.append(new ElementCreator("input").id("resource-domestic").with("type", "checkbox"));
+
+    /* In the end, we add the code to handle saving the resource on the server and terminating edit mode */
+    formCreator
+        .append(new ElementCreator("button").text("Speichern").listener('click', (event) => {
+            /* Why do we have to prevent the default action? Try commenting this line. */
+            //The default action of the browser for a form button is submit so we tell the browser that the form is handled explicitly
+            event.preventDefault();
+
+            /* The user saves the resource.
+               Task 4 - Part 2: We manually set the edited values from the input elements to the resource object. 
+               Again, this code here is just an example of how the name of our example resource can be obtained
+               and set in to the resource. The idea is that you handle your own properties here.
+            */
+            resource.name = document.getElementById("resource-name").value;
+            resource.species = document.getElementById("resource-species").value;
+            resource.age = document.getElementById("resource-age").value;
+            resource.birthday = document.getElementById("resource-birthday").value;
+            resource.isDomestic = document.getElementById("resource-domestic").checked;
+
+            /* Task 4 - Part 3: Call the update endpoint asynchronously. Once the call returns successfully,
+               use the code below to remove the form we used for editing and again render 
+               the resource in the list.
+            */
+            const xhr = new XMLHttpRequest();
+            xhr.open('PUT', `/api/resources/${resource.id}`);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    add(resource, document.getElementById(resource.idforDOM));  // <- Call this after the resource is updated successfully on the server
+                }
+            };
+            xhr.send(JSON.stringify(resource));
+        }))
+        .replace(document.querySelector('main'), document.getElementById(resource.idforDOM));
+}
+
+function remove(resource) {
+    document.getElementById(resource.idforDOM).remove();
+}
+
+/* Task 5 - Create a new resource is very similar to updating a resource. First, you add
+   an empty form to the DOM with the exact same fields you used to edit a resource.
+   Instead of PUTing the resource to the server, you POST it and add the resource that
+   the server returns to the DOM (Remember, the resource returned by the server is the
+    one that contains an id).
+ */
+function create() {
+    const formCreator = new ElementCreator("form")
+        .id("create-form")
+        .append(new ElementCreator("h3").text("Add Animal"))
+        .append(new ElementCreator("label").text("Name").with("for", "resource-name"))
+        .append(new ElementCreator("input").id("resource-name").with("type", "text"))
+        .append(new ElementCreator("label").text("Species").with("for", "resource-species"))
+        .append(new ElementCreator("input").id("resource-species").with("type", "text"))
+        .append(new ElementCreator("label").text("Age").with("for", "resource-age"))
+        .append(new ElementCreator("input").id("resource-age").with("type", "text"))
+        .append(new ElementCreator("label").text("Birthday").with("for", "resource-birthday"))
+        .append(new ElementCreator("input").id("resource-birthday").with("type", "date"))
+        .append(new ElementCreator("label").text("Domestic").with("for", "resource-domestic"))
+        .append(new ElementCreator("input").id("resource-domestic").with("type", "checkbox"))
+        .append(new ElementCreator("button").text("Speichern").listener('click', (event) => {
+            event.preventDefault();
+
+            const newAnimal = {
+                name: document.getElementById("resource-name").value,
+                species: document.getElementById("resource-species").value,
+                age: document.getElementById("resource-age").value,
+                birthday: document.getElementById("resource-birthday").value,
+                isDomestic: document.getElementById("resource-domestic").checked
+            };
+
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', `/api/resources`);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    add(Object.assign(new Animal(), JSON.parse(xhr.responseText)));
+                    document.getElementById("create-form").remove();
+                }
+            };
+            xhr.send(JSON.stringify(newAnimal));
+        }));
+
+    const parent = document.querySelector('main');
+    formCreator.insertBefore(parent, document.querySelector('#bottom'));
+}
+
+    
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    fetch("/api/resources")
+        .then(response => response.json())
+        .then(resources => {
+            for (const resource of resources) {
+                add(Object.assign(new Animal(), resource));
+            }
+        });
+});
+
